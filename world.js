f404a753e8e347aa83c535ac6e0f8898d348c01c
Nandon <nandonarts@gmail.com>
2022-11-17 01:02:23 -0400

    Added Ability to search for cities of a country

--- world.js
+++ world.js
@@ -1,14 +1,17 @@
 window.addEventListener('DOMContentLoaded',(event)=>{
 
-   let  btn = document.querySelector("#lookup")
-    let unit =document.querySelector("#country")
+   let  btn = document.querySelector("#lookup");
+   let btn2=document.querySelector("#finding");
+    let unit =document.querySelector("#country");
     let A ="http://localhost/comp2245-assignment5/world.php?country="
-    btn.onclick=function(){Retriv()}
+    let B ="&lookup="
+    btn.onclick=function(){Retriv()};
+    btn2.onclick=function(){Retriv("cities")};
     const httpRequest = new XMLHttpRequest();
-    function Retriv(){
-
+    function Retriv(Cit){
+            
         httpRequest.onreadystatechange = DoIt();
-        httpRequest.open('GET', A+unit.value);
+        httpRequest.open('GET', A+unit.value+B+Cit);
         httpRequest.send();
 
     }
--- world.php
+++ world.php
@@ -6,22 +6,72 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $Count_IN = $_GET['country'];
+$Cit=$_GET['lookup'];
 
-
+Detect($Count_IN,$Cit,$conn);
+function Detect($Count_IN,$Cit,$conn){
+  if($Count_IN=="")
+  {
+    if($Cit=="cities"){
+      $stmt =  $conn->query("SELECT cities.name AS CN, district, cities.population AS Pop FROM cities JOIN countries ON countries.code = cities.country_code");  
+      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      Districts($results);
+      return;
+    }  
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      CountIN($results);
+      return;
+  }
+  if ($Cit=="cities"){
+    $stmt =  $conn->query("SELECT cities.name AS CN, district, cities.population AS Pop  FROM cities JOIN countries ON countries.code = cities.country_code WHERE countries.name LIKE '%$Count_IN%' ");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    Districts($results);
+    return;
+  }
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$Count_IN%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  CountIN($results);
   
-  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$Count_IN%' ");
+}
+function CountIN($results){
 
-  if($Count_IN ="")
-    {
-      $stmt = $conn->query("SELECT * FROM countries");
-    }
+?> 
+<table>
+<tr>
+<th>Name</th>
+<th>Continent</th>
+<th>Independence Year</th>
+<th>Head of State</th>
+</tr>
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-?>
-<ul>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<tr>
+  <td><?=$row['name'] ?></td>
+  <td><?=$row['continent'] ?></td>
+  <td><?=$row['independence_year'] ?></td>
+  <td><?=$row['head_of_state'] ?></td>
+</tr>  
+<?php endforeach;?>
+</table>
+<?php
+}
+function Districts($results){ ?>
+  <table>
+  <tr>
+  <th>Name</th>
+  <th>District</th>
+  <th>Population</th>
+  </tr>
+  
+  <?php foreach ($results as $row): ?>
+  <tr>
+    <td><?=$row['CN'] ?></td>
+    <td><?=$row['district'] ?></td>
+    <td><?=$row['Pop'] ?></td>
+  </tr>  
+  <?php endforeach;?>
+  </table>
+<?php }
+?>
+
